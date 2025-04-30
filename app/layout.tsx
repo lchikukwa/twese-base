4d1edd9963156e6ab0af813d79f21ef6007fa3cd
Langton Chikukwa <chikukwa@icloud.com>
2025-04-30 12:37:03 +0200

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "@/public/css/lux-bootstrap.min.css";
 import "@/public/css/whatsapp-floating.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import type { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Twese Mart",
@@ -15,6 +16,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+          strategy="beforeInteractive"
+        />
+        <Script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+          strategy="beforeInteractive"
+        />
+      </head>
       <body className={``}>
         {children}
         <a
